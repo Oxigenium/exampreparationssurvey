c2cab10bb362a68562cf5cc32b6fda4089dca908
Oxigenium <alek.c20@mail.ru>
2022-11-28 15:38:36 +0200

    escape left bracket before markdown

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ function getFullCorrectAnswers(answers, variants) {
 }
 
 function escapeString(json) {
-  var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+  var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff<]/g;
   var meta = {
     // table of character substitutions
     "\b": "\\b",
@@ -186,6 +186,7 @@ function escapeString(json) {
     "\f": "\\f",
     "\r": "\\r",
     '"': '\\"',
+    '<': '&lt;',
     "\\": "\\\\"
   };
 
